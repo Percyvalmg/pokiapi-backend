8d014a6d1977fa40daa0888568ddfe8714e820e7
Percyval Gonde <percyval@yelah.co.za>
2021-11-22 10:47:59 +0200

    Initial commit

---
+++ src/main/java/com/percyval/pokiapi/PokiapiApplication.java
@@ -0,0 +1,11 @@
+package com.percyval.pokiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokiapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PokiapiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/percyval/pokiapi/PokiapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.percyval.pokiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokiapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
